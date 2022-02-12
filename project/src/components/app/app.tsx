60714be8d792e8e6bae487c927fcf468cd326f9d
enfllome <enfllome.98@mail.ru>
2022-02-12 14:14:15 +0300

    Добавит компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,23 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+// import SignIn from '../sign-in/sign-in';
+// import Favorites from '../favorites/favorites';
+// import FavoritesEmpty from '../favorites-empty/favorites-empty';
+// import Property from '../property/property';
+
+type AppProps = {
+  offersCount: number,
+};
+
+function App({ offersCount }: AppProps): JSX.Element {
+  return (
+    <Main
+      offersCount={offersCount}
+    />
+    // <SignIn />
+    // <Favorites />
+    // <FavoritesEmpty />
+    // <Property />
+  );
 }
 
 export default App;
