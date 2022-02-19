85edceb4c4c731e7bbc2c20b69601057f44d3119
enfllome <enfllome.98@mail.ru>
2022-02-19 18:55:55 +0300

    Добавит компонент NavItems

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,4 +1,6 @@
+import { AppRoute } from '../../const';
 import Logo from '../logo/logo';
+import NavItems from '../nav-items/nav-items';
 
 function Header ():JSX.Element {
   return (
@@ -9,24 +11,7 @@ function Header ():JSX.Element {
             <Logo width='81' height='41' />
           </div>
           {
-            window.location.pathname === '/login'
-              ? null
-              :
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <a className="header__nav-link header__nav-link--profile" href="#">
-                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    </a>
-                  </li>
-                  <li className="header__nav-item">
-                    <a className="header__nav-link" href="#">
-                      <span className="header__signout">Sign out</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+            window.location.pathname !== AppRoute.Login && <NavItems />
           }
         </div>
       </div>
