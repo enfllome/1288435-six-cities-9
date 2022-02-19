cb60ecd6e2acebf9b53e86b17d8d593e2bdf02bb
enfllome <enfllome.98@mail.ru>
2022-02-19 20:59:43 +0300

    Исправит роут

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 type LogoProps = {
   width: string,
@@ -7,7 +8,7 @@ type LogoProps = {
 
 function Logo ({ width, height }: LogoProps):JSX.Element {
   return (
-    <Link className="header__logo-link" to="/">
+    <Link className="header__logo-link" to={AppRoute.Root}>
       <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width={width} height={height} />
     </Link>
   );
