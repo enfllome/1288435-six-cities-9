04a274e685e38a3bdab89e090020b5f3c88b0ab6
enfllome <enfllome.98@mail.ru>
2022-02-19 17:18:00 +0300

    Добавит маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,11 @@
 import Main from '../main/main';
-// import SignIn from '../sign-in/sign-in';
-// import Favorites from '../favorites/favorites';
-// import FavoritesEmpty from '../favorites-empty/favorites-empty';
-// import Property from '../property/property';
+import SignIn from '../sign-in/sign-in';
+import Favorites from '../favorites/favorites';
+import Property from '../property/property';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   offersCount: number,
@@ -10,13 +13,36 @@ type AppProps = {
 
 function App({ offersCount }: AppProps): JSX.Element {
   return (
-    <Main
-      offersCount={offersCount}
-    />
-    // <SignIn />
-    // <Favorites />
-    // <FavoritesEmpty />
-    // <Property />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<Main offersCount={offersCount}/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              autorizationStatus={AuthorizationStatus.Auth}
+            >
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<Property />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<SignIn />}
+        />
+        <Route
+          path='*'
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
