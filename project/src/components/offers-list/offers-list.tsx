c8ed87c6c751145b394ca4980597ff8e59ce4be9
enfllome <enfllome.98@mail.ru>
2022-02-25 21:40:37 +0300

    Сократит передачу пропсов остаточными параметрами

--- project/src/components/offers-list/offers-list.tsx
+++ project/src/components/offers-list/offers-list.tsx
@@ -10,7 +10,7 @@ function OffersList ({ offers }: OffersListProps):JSX.Element {
     <div className="cities__places-list places__list tabs__content">
       {
         offers.map((offer) => (
-          <OfferItem key={offer.id} previewImage={offer.previewImage} price={offer.price} title={offer.title} type={offer.type} id={offer.id} />
+          <OfferItem key={offer.id} {...offer} />
         ))
       }
     </div>
