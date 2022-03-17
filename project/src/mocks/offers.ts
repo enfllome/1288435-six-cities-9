6cdbc74c547db7789682835b3ec8a745d845f8f6
enfllome <enfllome.98@mail.ru>
2022-03-17 23:55:11 +0300

    Изменит моки

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -17,7 +17,7 @@ export const offers: Offer[] = [
         longitude: 4.85309666406198,
         zoom: 10,
       },
-      name: 'Amsterdam',
+      name: 'Paris',
     },
   },
   {
@@ -36,7 +36,7 @@ export const offers: Offer[] = [
         longitude: 4.85309666406198,
         zoom: 10,
       },
-      name: 'Amsterdam',
+      name: 'Paris',
     },
   },
   {
