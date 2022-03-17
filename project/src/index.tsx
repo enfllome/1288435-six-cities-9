df84d53a5838a026ac8affc408dac609024568b1
enfllome <enfllome.98@mail.ru>
2022-03-17 23:53:02 +0300

    Подключит redux

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
-import { comments } from './mocks/comments';
-import { offers } from './mocks/offers';
+import { store } from './store';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App comments={comments} offers={offers}/>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
