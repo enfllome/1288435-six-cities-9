65f1d11fff37f03f612c126a4eb715b80867df2e
enfllome <enfllome.98@mail.ru>
2022-03-31 22:55:08 +0300

    Рефакторит PrivateRoute

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -7,13 +7,11 @@ import { AppRoute } from '../../const';
 import PrivateRoute from '../private-route/private-route';
 import PropertyLogic from '../property-logic/property-logic';
 import { useAppSelector } from '../../hooks';
-import { getAutorizationStatus, getDataLoaded } from '../../store/selectors';
+import { getDataLoaded } from '../../store/selectors';
 import LoadingScreen from '../loading-screen/loading-screen';
 
 function App(): JSX.Element {
-
   const isDataLoaded = useAppSelector(getDataLoaded);
-  const autorizationStatus = useAppSelector(getAutorizationStatus);
 
   if (!isDataLoaded) {
     return (
@@ -31,9 +29,7 @@ function App(): JSX.Element {
         <Route
           path={AppRoute.Favorites}
           element={
-            <PrivateRoute
-              autorizationStatus={autorizationStatus}
-            >
+            <PrivateRoute>
               <Favorites />
             </PrivateRoute>
           }
--- project/src/components/private-route/private-route.tsx
+++ project/src/components/private-route/private-route.tsx
@@ -1,13 +1,15 @@
 import { Navigate } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../const';
+import { useAppSelector } from '../../hooks';
+import { getAutorizationStatus } from '../../store/selectors';
 
 type PrivateRouteProps = {
-  autorizationStatus: AuthorizationStatus;
   children: JSX.Element;
 }
 
 function PrivateRoute (props: PrivateRouteProps):JSX.Element {
-  const { autorizationStatus, children } = props;
+  const { children } = props;
+  const autorizationStatus = useAppSelector(getAutorizationStatus);
   return (
     autorizationStatus === AuthorizationStatus.Auth
       ? children
